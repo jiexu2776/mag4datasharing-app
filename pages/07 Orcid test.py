297cc74012d262bbd0af7c9ba04441bd32d14de8
Jie Xu <xu@em.uni-frankfurt.de>
2025-11-13 11:35:21 +0100

    Update 07 Orcid test.py

--- pages/07 Orcid test.py
+++ pages/07 Orcid test.py
@@ -13,9 +13,10 @@ import requests
 import streamlit as st
 
 if not st.user.is_logged_in:
-    if st.button("Log in with Google"):
+    if st.button("Log in with ORCID"):
         st.login()
     st.stop()
+
 
 st.button("Log out", on_click=st.logout)
 st.markdown(f"Welcome! {st.user.name}")
