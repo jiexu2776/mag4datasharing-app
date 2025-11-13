b976eb0264f1b87d15d42b044e38b8ecb8ed5d58
Jie Xu <xu@em.uni-frankfurt.de>
2025-11-13 15:07:03 +0100

    Update 04 Prepare your file for upload.py

--- pages/04 Prepare your file for upload.py
+++ pages/04 Prepare your file for upload.py
@@ -4,7 +4,13 @@ st.title('How to prepare your dataset before upload')
 
 st.write('Needs to be a csv file')
 
-if st.session_state.is_authenticated == True:
-    st.header('Test')
+if st.user.is_logged_in:
+    st.sidebar.success("You are logged in with ORCID")
 else:
-    st.subheader('Login with ORCID to see content')
+    st.sidebar.error('You are not loged in to ORCID')
+if st.sidebar.button("Log out"):
+    st.logout()
+
+if st.sidebar.button("Log out"):
+    st.logout()
+
