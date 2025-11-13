68b0787ae7300f3819b72e813a0636384773b8a0
Jie Xu <xu@em.uni-frankfurt.de>
2025-11-13 15:00:16 +0100

    Update 02 File upload to mag4.py

--- pages/02 File upload to mag4.py
+++ pages/02 File upload to mag4.py
@@ -160,13 +160,13 @@ if uploaded_file is not None:
         st.session_state.all_metadata_added = False
 
     if st.button("Upload to mag4", disabled=st.session_state.all_metadata_added):
-        response = upload_to_github(file_path_user_dataset, str(st.session_state.orcid_user_info['sub']), 'csv')
+        response = upload_to_github(file_path_user_dataset, str(st.user['sub']), 'csv')
         if response.status_code == 201:
             st.success(f"Dataset successfully uploaded to mag4datasets.")
         else:
             st.error(f"Error uploading file to GitHub. Status Code: {response.status_code}, Response: {response.text}")
 
-        response = upload_to_github(file_path_json_metadata, str(st.session_state.orcid_user_info['sub']), 'json')
+        response = upload_to_github(file_path_json_metadata, str(st.user['sub']), 'json')
         if response.status_code == 201:
             st.success(f"Metadata successfully uploaded to mag4datasets.")
         else:
