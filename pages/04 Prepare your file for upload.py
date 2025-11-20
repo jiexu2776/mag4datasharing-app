72bb8fac58ea163fb63103f3aafb9f9eebbe565c
Jie Xu <xu@em.uni-frankfurt.de>
2025-11-20 10:55:44 +0100

    Update 04 Prepare your file for upload.py

--- pages/04 Prepare your file for upload.py
+++ pages/04 Prepare your file for upload.py
@@ -2,7 +2,23 @@ import streamlit as st
 
 st.title('How to prepare your dataset before upload')
 
-st.write('Needs to be a csv file')
+st.markdown(
+    """
+## 🧩 How to prepare your dataset
+
+Your file must be provided as a **CSV**.
+
+For best results, we encourage you to prepare your dataset following the
+guidelines from **[EarthChem](https://earthchem.org/)**.
+
+Using the official **[EarthChem templates](https://earthchem.org/ecl/templates)** helps keep your data organized and makes it much easier to publish your dataset and obtain a DOI later.
+
+Simply download the template that matches your data type, fill it in, and upload it here.
+"""
+)
+
+
+
 
 if st.user.is_logged_in:
     st.sidebar.success("You are logged in with ORCID")
