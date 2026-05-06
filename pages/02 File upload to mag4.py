6bdff0dba64880187886be65165e760585ee0f90
Jie Xu <xu@em.uni-frankfurt.de>
2026-05-06 16:24:55 +0200

    Update 02 File upload to mag4.py

--- pages/02 File upload to mag4.py
+++ pages/02 File upload to mag4.py
@@ -92,6 +92,11 @@ if uploaded_file is not None:
 
 
 # ---------- Metadata Fields
+    "Name": (
+    st.user.get("name") or 
+    f"{st.user.get('given_name', '')} {st.user.get('family_name', '')}".strip() or 
+    st.user.get("sub")
+    )
     st.header('Metadata')
     st.subheader('Mandatory')
     st.text_input('ORCID', st.user['sub'], disabled=True)
